2e0a1d9694470d277f2ab0362c07306b9d44fb99
Hytn <ht1ian.chen@gmail.com>
2024-08-18 21:19:34 +0800

    add config.py

---
+++ config/config.py
@@ -0,0 +1,9 @@
+# This file is ...
+# Author: Haotian Chen, ...
+# Date: 2024-08
+# Copyright (c) THUNLP, Tsinghua University. All rights reserved. 
+# See LICENSE file in the project root for license information. 
+
+
+
+
