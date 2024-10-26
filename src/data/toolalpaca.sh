ffd15ed67a17a956ac377eb5252497df60e7544c
Leonard907 <ou_litu@163.com>
2024-10-26 18:40:42 +0000

    ToolAlpaca done with newest specification

--- src/data/toolalpaca.sh
+++ src/data/toolalpaca.sh
@@ -1,13 +1,9 @@
 git clone https://github.com/Leonard907/ToolAlpaca.git
-cd ToolAlpaca
-python build_dataset.py -api data/train_data.json -out ../toolalpaca_train.json
-python build_dataset.py -api data/eval_simulated.json -out ../toolalpaca_val_sim.json -eval
-python build_dataset.py -api data/eval_real.json -out ../toolalpaca_val_real.json -eval
-cd ..
+# cd ToolAlpaca
 python process_toolalpaca.py
 # clean up
-rm -rf ToolAlpaca
 mkdir toolalpaca_data
-mv toolalpaca_train.json toolalpaca_data/
-mv toolalpaca_val_sim.json toolalpaca_data/
-mv toolalpaca_val_real.json toolalpaca_data/
+mv toolalpaca_eval_simulated.json toolalpaca_data/toolalpaca_eval_simulated_sharegpt.json
+mv toolalpaca_eval_real.json toolalpaca_data/toolalpaca_eval_real_sharegpt.json
+mv toolalpaca_train.json toolalpaca_data/toolalpaca_train_sharegpt.json
+rm -rf ToolAlpaca
