981e24c5c07611bdb58d227a811ec6b37b8f6f6d
Zijun Song <97300698+ZijunSong@users.noreply.github.com>
2024-10-30 13:07:47 +0800

    Update RestGPT.sh

--- src/data/RestGPT.sh
+++ src/data/RestGPT.sh
@@ -1,10 +1,10 @@
-git clone https://https://github.com/Yifan-Song793/RestGPT.git
+git clone https://github.com/Yifan-Song793/RestGPT.git
 cd RestGPT
 
 shopt -s extglob
 rm -rf !(datasets)
 
-mv data ../../BodhiAgent/src/RestGPT
+mv datasets ../../data/RestGPT_data
 
 cd ..
 rm -rf RestGPT
