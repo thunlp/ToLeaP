d46f7ac0c2348382c03e220b055b99063a1b3a45
Zijun Song <97300698+ZijunSong@users.noreply.github.com>
2024-10-30 13:22:56 +0800

    Update Toolllm.sh

--- src/data/Toolllm.sh
+++ src/data/Toolllm.sh
@@ -1,8 +1,7 @@
-wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1XFjDxVZdUY7TXYF2yvzx3pJlS2fy78jk&confirm=yes' -O data.zip
-unzip data.zip
-rm data.zip
-mv data toolllm_data
-cd toolllm_data
+sed -i 's/\r$//' Toolllm.sh
+pip install -q gdown
+gdown "https://drive.google.com/uc?export=download&id=1XFjDxVZdUY7TXYF2yvzx3pJlS2fy78jk"
+unzip -o data.zip -d toolllm_data
 
 # Toolllama_G123_defs_train.json is the main training data for ToolLLaMA
 # It does not conform to the data format of Sharegpt Format Supervised Fine Tuning Dataset
