811b45b7db3ab63d400f71bf4fa60686795d3c0c
Leonard907 <ou_litu@163.com>
2024-10-30 10:53:47 +0000

    move data to sft_data

--- src/data/toolalpaca.sh
+++ src/data/toolalpaca.sh
@@ -2,8 +2,8 @@ git clone https://github.com/Leonard907/ToolAlpaca.git
 # cd ToolAlpaca
 python process_toolalpaca.py
 # clean up
-mkdir toolalpaca_data
-mv toolalpaca_eval_simulated.json toolalpaca_data/toolalpaca_eval_simulated_sharegpt.json
-mv toolalpaca_eval_real.json toolalpaca_data/toolalpaca_eval_real_sharegpt.json
-mv toolalpaca_train.json toolalpaca_data/toolalpaca_train_sharegpt.json
+mkdir -p sft_data
+mv toolalpaca_eval_simulated.json sft_data/toolalpaca_eval_simulated_sharegpt.json
+mv toolalpaca_eval_real.json sft_data/toolalpaca_eval_real_sharegpt.json
+mv toolalpaca_train.json sft_data/toolalpaca_train_sharegpt.json
 rm -rf ToolAlpaca
