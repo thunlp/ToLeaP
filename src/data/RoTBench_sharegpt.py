4e3390b618d00704b022a4870ef0be82232145ea
ColaZhang22 <aqzk643788315@outlook.com>
2024-11-01 20:48:34 +0900

    update

--- src/data/RoTBench_sharegpt.py
+++ src/data/RoTBench_sharegpt.py
@@ -36,22 +36,35 @@ def transferSharegpt(data):
     return data
 
 def transferSharegptthrid(data):
+
     for conversationIndex in data:
         del conversationIndex["id"]
         del conversationIndex["scenario"]
-        rawData=conversationIndex["conversations"]
+        rawData=copy.deepcopy(conversationIndex["conversations"])
         # print(type(rawData))
         #start to modify
-        for i in rawData:   
-            if i["from"] == "assistant":
-                i["from"] = "gpt"
-            elif i["from"] == "user":
-                i["from"] = "human"
-            elif i["from"] == "system":
-                conversationIndex["system"] = i["value"]
-                del conversationIndex["conversations"][0]
-            elif i["from"] == "function":
-                i["from"] = "observation"
+        index = 2
+        for i in range(len(rawData)):   
+            if rawData[i]["from"] == "assistant":
+                #  try to deal value
+                if type(rawData[i]["value"]) == list:
+                    for thought in rawData[i]["value"]:
+                        insert(conversationIndex, thought, index)
+                    next(i)
+                elif type(rawData[i]["value"]) == str:
+                    thought = rawData[i]["value"]
+                    insert(conversationIndex, thought, index)
+                    next(i)
+                del conversationIndex["conversations"][index+2]
+                index +=2
+            elif rawData[i]["from"] == "user":
+                conversationIndex["conversations"][i]["from"] = "human"
+            elif rawData[i]["from"] == "system":
+                conversationIndex["system"] = rawData[i]["value"]
+            elif rawData[i]["from"] == "function":
+                conversationIndex["conversations"][i]["from"] = "function_call"
+        del conversationIndex["conversations"][0]
+   
     return data
 
 if __name__ == "__main__":
@@ -66,36 +79,36 @@ if __name__ == "__main__":
 
     # 获取当前文件夹和当前文件夹下所有文件夹
     current_dir = os.getcwd()
-    save_dir = "../../sft_data"
+    save_dir = "../../RoTBenchData/"
     folders = [f for f in os.listdir(current_dir) if os.path.isdir(os.path.join(current_dir, f))]
     # 获取所有的 .json 文件
     for current_dir in folders:
         json_files =  glob.glob(os.path.join(current_dir, "*.json"))
         # 读取每个 JSON 文件的内容
         for file_path in json_files:
-              
-                with open(file_path, 'r', encoding='utf-8') as f: 
-                    data = json.load(f)
-                    # 处理 first turn 文件
-                    if current_dir == "First_Turn":
-                        file_name_N = save_dir+ "//first_turn_new_" + os.path.basename(file_path)
-                        try:
-                            dataNew = transferSharegpt(data)
-                            with open(file_name_N, 'w', encoding='utf-8') as f:
-                                json.dump(dataNew, f, ensure_ascii=False, indent=4)
-                                print(file_path, ":Finished")
-                        except:
-                            print(file_path, ":Failed")
+            with open(file_path, 'r', encoding='utf-8') as f: 
+                data = json.load(f)
+                # 处理 first turn 文件
+                if current_dir == "First_Turn":
+                    file_name_N = save_dir+ "first_turn_new_" + os.path.basename(file_path)
+                    try:
+                        dataNew = transferSharegpt(data)
+                        for wrong in dataNew:
+                            del wrong["conversations"][-1]
+                        with open(file_name_N, 'w', encoding='utf-8') as f:
+                            json.dump(dataNew, f, ensure_ascii=False, indent=4)
+                            print(file_path, ":Finished")
+                    except:
+                        print(file_path, ":Failed")
 
                     # 处理 thrid turn 文件
-                    if current_dir == "Third_Turn":
-                        file_name_N = save_dir+ "//third_turn_new_" + os.path.basename(file_path)
-                        try:
-                            dataNew = transferSharegptthrid(data)
-                            with open(file_name_N, 'w', encoding='utf-8') as f:
-                                json.dump(dataNew, f, ensure_ascii=False, indent=4)
-                                print(file_path, ":Finished")
-                        except:
-                            print(file_path, ":Failed")
-
+                    # if current_dir == "Third_Turn":
+                    #     file_name_N = save_dir+ "//third_turn_new_" + os.path.basename(file_path)
+                    #     try:
+                    #         dataNew = transferSharegptthrid(data)
+                    #         with open(file_name_N, 'w', encoding='utf-8') as f:
+                    #             json.dump(dataNew, f, ensure_ascii=False, indent=4)
+                    #             print(file_path, ":Finished")
+                    #     except:
+                    #         print(file_path, ":Failed")
     print("Data Complete")
