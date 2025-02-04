e1703402b60056e6b5143fb7adbab099a1ff21e6
Zijun Song <97300698+ZijunSong@users.noreply.github.com>
2025-02-05 07:23:30 +0800

    Update one-click-evaluation.sh

--- src/scripts/one-click-evaluation.sh
+++ src/scripts/one-click-evaluation.sh
@@ -5,15 +5,15 @@ MODEL_PATH=$1
 # TaskBench
 python taskbench_eval.py \
     --model $MODEL_PATH \
-    --data_path /home/test/test03/szj/BodhiAgent-main/src/data/sft_data/TaskBench/taskbench_data_huggingface.json \
+    --data_path ../data/sft_data/TaskBench/taskbench_data_huggingface.json \
     --is_api False
 python taskbench_eval.py \
     --model $MODEL_PATH \
-    --data_path /home/test/test03/szj/BodhiAgent-main/src/data/sft_data/TaskBench/taskbench_data_dailylifeapis.json \
+    --data_path ../data/sft_data/TaskBench/taskbench_data_dailylifeapis.json \
     --is_api False
 python taskbench_eval.py \
     --model $MODEL_PATH \
-    --data_path /home/test/test03/szj/BodhiAgent-main/src/data/sft_data/TaskBench/taskbench_data_multimedia.json \
+    --data_path ../data/sft_data/TaskBench/taskbench_data_multimedia.json \
     --is_api False
 
 # RoTBench
@@ -38,6 +38,7 @@ bfcl evaluate \
 bash test_all_teval.sh vllm $MODEL_PATH QwQ-32B-Preview False
 
 # InjecAgent
+export OPENAI_API_KEY=
 python3 injecagent_eval.py \
     --model_type OpenModel \
     --model_name $MODEL_PATH \
