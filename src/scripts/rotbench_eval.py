ac0f13ef4144a7191eb179a983114cad005fb39f
Zijun Song <97300698+ZijunSong@users.noreply.github.com>
2025-03-18 14:13:31 +0800

    Update rotbench_eval.py

--- src/scripts/rotbench_eval.py
+++ src/scripts/rotbench_eval.py
@@ -448,8 +448,7 @@ def main(
             else: # if not 
                 if not conf.use_chat: # hf batch generate
                     results = llm.batch_generate_complete(
-                        [(ed["conversations"][0]["value"] + ed["conversations"][1]["value"]) for ed in eval_data],
-                        temperature=0
+                        [(ed["conversations"][0]["value"] + ed["conversations"][1]["value"]) for ed in eval_data]
                     )
                 else:  # vllm batch generate
                     messages = create_messages(eval_data)
