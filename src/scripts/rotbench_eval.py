efb71a5086e84e6603dfd0a0746de2f79d8f7025
Zijun Song <97300698+ZijunSong@users.noreply.github.com>
2025-04-26 15:21:37 +0800

    Update rotbench_eval.py

--- src/scripts/rotbench_eval.py
+++ src/scripts/rotbench_eval.py
@@ -1,3 +1,18 @@
+# Copyright 2024 Junjie-Ye/RoTBench
+# Modifications Copyright 2024 BodhiAgent
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sys
 import os
 import click
@@ -5,12 +20,8 @@ import json
 from typing import List, Dict
 from ast import literal_eval
 
-current_dir = os.path.dirname(os.path.abspath(__file__)) 
-utils_dir = os.path.join(current_dir, '..')
-sys.path.append(utils_dir)
-
 from cfg.config import Config
-from benchmark_utils.llm import LLM
+from utils.llm import LLM
 
 def create_messages(conversation_data: List[Dict]) -> List[List[Dict]]:
     messages = []
