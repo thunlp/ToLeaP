11bee5425f8d5473538c8bc73e6b731ee204b460
MuL1ian <boye.niu@sydney.edu.au>
2025-04-27 10:35:10 +0800

    delete non relative part - model

--- src/model/workflow.py
+++
@@ -1,7 +0,0 @@
-
-
-class AIDE:
-    def __init__(self):
-        pass
-
-
--- src/scripts/teval_setup.sh
+++ src/scripts/teval_setup.sh
@@ -1,3 +1,9 @@
+# This file is to set up the T-Eval environment easily.
+# Author: Boye Niu
+# Date: 2025-04
+# Copyright (c) THUNLP, Tsinghua University. All rights reserved.
+# See LICENSE file in the project root for license information.
+
 set -e  
 
 echo "Creating T-Eval folder and moving files..."
