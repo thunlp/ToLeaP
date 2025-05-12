8d617140ba09ff92e50406a725bc9d7734e75855
Purple Nightmare <97300698+ZijunSong@users.noreply.github.com>
2025-05-12 13:46:01 +0800

    Fix some bugs

--- scripts/one-click-evaluation.sh
+++ scripts/one-click-evaluation.sh
@@ -8,32 +8,60 @@
 
 MODEL_PATH=$1
 IS_API=$2
-MODEL_NAME=$(basename $MODEL_PATH)  
+GPU_NUM=$3
+BATCH_SIZE=$4
+INPUT_TOKENS=$5
+OUTPUT_TOKENS=$6
+if [ -n "$7" ]; then
+  MODEL_NAME="$7"
+else
+  MODEL_NAME=$(basename "$MODEL_PATH")
+fi
+
 OUTPUT_FILE="${MODEL_NAME}_results.json"  
 
 > $OUTPUT_FILE
 
-# TaskBench
+echo "********** doing evaluation by $MODEL_NAME on TaskBench benchmark **********"
 python taskbench_eval.py \
     --model $MODEL_PATH \
-    --is_api $IS_API | grep -oP '\{.*\}' >> $OUTPUT_FILE
+    --is_api $IS_API \
+    --tensor_parallel_size $GPU_NUM \
+    --batch_size $BATCH_SIZE \
+    --max_model_len $INPUT_TOKENS \
+    --model_name $MODEL_NAME \
+    --max_output_tokens $OUTPUT_TOKENS | grep -oP '\{.*\}' >> $OUTPUT_FILE
 
-# SealTools
+echo "********** doing evaluation by $MODEL_NAME on SealTools benchmark **********"
 python sealtools_eval.py \
     --model $MODEL_PATH \
-    --is_api $IS_API | grep -oP '\{.*\}' >> $OUTPUT_FILE
+    --is_api $IS_API \
+    --tensor_parallel_size $GPU_NUM \
+    --batch_size $BATCH_SIZE \
+    --max_model_len $INPUT_TOKENS \
+    --model_name $MODEL_NAME \
+    --max_output_tokens $OUTPUT_TOKENS | grep -oP '\{.*\}' >> $OUTPUT_FILE
 
-# RoTBench
+echo "********** doing evaluation by $MODEL_NAME on RoTBench benchmark **********"
 python rotbench_eval.py \
     --model $MODEL_PATH \
-    --is_api $IS_API | grep -oP '\{.*\}' >> $OUTPUT_FILE
+    --is_api $IS_API \
+    --tensor_parallel_size $GPU_NUM \
+    --batch_size $BATCH_SIZE \
+    --max_model_len $INPUT_TOKENS \
+    --model_name $MODEL_NAME \
+    --max_output_tokens $OUTPUT_TOKENS | grep -oP '\{.*\}' >> $OUTPUT_FILE
 
-# Glaive
+echo "********** doing evaluation by $MODEL_NAME on Glaive benchmark **********"
 python glaive_eval.py \
     --model $MODEL_PATH \
-    --is_api $IS_API | grep -oP '\{.*\}' >> $OUTPUT_FILE
+    --is_api $IS_API \
+    --tensor_parallel_size $GPU_NUM \
+    --batch_size $BATCH_SIZE \
+    --max_model_len $INPUT_TOKENS \
+    --max_output_tokens $OUTPUT_TOKENS | grep -oP '\{.*\}' >> $OUTPUT_FILE
 
-# InjecAgent
+echo "********** doing evaluation by $MODEL_NAME on InjecAgent benchmark **********"
 if [ "$IS_API" == "True" ]; then
     MODEL_TYPE=GPT
 else
@@ -43,22 +71,26 @@ export OPENAI_API_KEY="your-open-api-kei"
 python injecagent_eval.py \
     --model_type $MODEL_TYPE \
     --model_name $MODEL_PATH \
+    --tensor_parallel_size $GPU_NUM \
+    --batch_size $BATCH_SIZE \
     --use_cach | grep -oP '\{.*\}' >> $OUTPUT_FILE
 
-# BFCL
+echo "********** doing evaluation by $MODEL_NAME on T-Eval benchmark **********"
+bash teval_eval.sh $MODEL_PATH $MODEL_NAME $IS_API $GPU_NUM
+
+echo "********** doing evaluation by $MODEL_NAME on APIBank benchmark **********"
+python apibank_eval.py \
+    --model $MODEL_PATH \
+    --is_api $IS_API \
+    --tensor_parallel_size $GPU_NUM \
+    --batch_size $BATCH_SIZE \
+    --max_model_len $INPUT_TOKENS \
+    --max_output_tokens $OUTPUT_TOKENS
+
+echo "********** doing evaluation by $MODEL_NAME on BFCL benchmark **********"
 bfcl generate \
     --model $MODEL_PATH \
     --test-category parallel,multiple,simple,parallel_multiple,java,javascript,irrelevance,multi_turn \
     --num-threads 1
 bfcl evaluate \
-    --model $MODEL_PATH >> "BFCL_results.txt"
-python standard_bfcl.py | grep -oP '\{.*\}' >> $OUTPUT_FILE
-
-# Skythought
-bash one-click-sky.sh $MODEL_PATH | grep -oP '\{.*\}' >> $OUTPUT_FILE
-
-# T-Eval
-cd T-Eval
-bash test_all_teval.sh $MODEL_PATH $MODEL_NAME $IS_API
-cd ..
-python standard_teval.py T-Eval/work_dirs/$MODEL_NAME/${MODEL_NAME}_-1_.json
+    --model $MODEL_PATH
